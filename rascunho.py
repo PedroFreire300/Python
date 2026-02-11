26d2defa2bb82405b0d73ec5f597a96a27d085c3
Pedro Freire <freire@vipsoft.local>
2026-02-11 17:17:05 -0300

    Pagina rasgunho para testes

---
+++ rascunho.py
@@ -0,0 +1,8 @@
+meta = 50000
+
+qtd_vendida = float(input("Qual foi a quantidade vendida?"))
+
+if qtd_vendida > meta:
+    print("A meta de vendas foi ultrapassada")
+else:
+    print("A meta de vendas NÃO foi ultrapassada")
