6ac8afd7dc35a6380c9293d3c0116eed11633f81
Pedr0Freire300 <pedro.freire4@fatec.sp.gov.br>
2026-02-25 21:18:45 -0300

    Praticando estrutura for

--- rascunho.py
+++ rascunho.py
@@ -1,4 +1,12 @@
-numeros = [10, 20, 30, 40, 50]
+meta = 10000
+vendas = [ ['João', 15000], ['Julia', 27000], ['Marcus', 9900], ['Maria', 3750], ['Ana', 10300],['Alon', 7870], ]
+#seu código aqui
 
-for numero in numeros:
-    print(numero*2)
+qtd_vendedores = 0
+
+for vendedores in vendas :
+    if vendedores[1]>meta:
+        
+        qtd_vendedores += 1
+
+print(qtd_vendedores)
