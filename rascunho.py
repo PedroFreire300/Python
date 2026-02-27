9a0be826d4631a8aa28f8bc26be90558d25f0d9b
Pedro Freire <freire@vipsoft.local>
2026-02-27 17:14:49 -0300

    Tentei estudar hoje...muita coisa para assimilar

--- rascunho.py
+++ rascunho.py
@@ -1,14 +1,1 @@
-qtd_convidados = int(input("Qual a quantidade convidados?"))
-
-lista = []
-
-for i in range(qtd_convidados):
-    nomes = input(f"Nome do convidado {i}: " )
-
-    lista.append(nomes)
-
-print(lista)
-
-
-
 
